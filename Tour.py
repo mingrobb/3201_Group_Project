6d1872eb720a2fd0601ab8987c647260d083ac4c
Weiming Chen <wc5868@mun.ca>
2018-11-15 14:02:22 -0330

    Create Mutation.py, Update Tour.py
    Cretae Mutation method
    Update Tour.py: add max distance and worst index in a tour object

--- Tour.py
+++ Tour.py
@@ -17,6 +17,8 @@ class Tour(object):
         self.city_map = city_map
         self.cities = city_map.keys()
         self.city_objects = []
+        self.max_distance = 0
+        self.worst_idx = []
         if tour:
             self.tour = tour
             self.length = self.cal_len()
@@ -28,7 +30,7 @@ class Tour(object):
 
     def cal_len(self):
         """
-        A function to calculate the length of a tour
+        A function to calculate the length of a tour and the max distance
         :return: the total length of the tour
         """
         tour_length = 0.0
@@ -39,12 +41,18 @@ class Tour(object):
             if city != self.tour[-1]:
                 next_city = self.tour[self.tour.index(city)+1]
                 distance_to_next = self.city_map[city].distances[next_city]
+                if distance_to_next > self.max_distance:
+                    self.max_distance = distance_to_next
+                    self.worst_idx = [self.tour.index(city), self.tour.index(city)+1]
                 tour_length += distance_to_next
 
             else:
                 #add the distance between the last city and the first city
                 start_city = self.tour[0]
                 distance_to_next = self.city_map[city].distances[start_city]
+                if distance_to_next > self.max_distance:
+                    self.max_distance = distance_to_next
+                    self.worst_idx = [self.tour.index(city), 0]
                 tour_length += distance_to_next
 
         return tour_length
@@ -64,7 +72,16 @@ print(tour.length)
 print(tour.tour)
 print(len(tour.tour))
 print(len(tour.city_objects))
+""
 for k,v in tour.city_map.items():
     print(v.id, v.x, v.y)
     print(v.distances)
+""
+print()
+print(tour.worst_idx)
+print(tour.max_distance)
+x = tour.city_objects[tour.worst_idx[0]].id
+y = tour.city_objects[tour.worst_idx[1]].id
+print(x, y)
+print(city_map[x].distances[y])
 """
